6499223daea7a6d4391578786f3738451447f342
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2020-05-09 15:13:15 +0600

    implementation

---
+++ A - Circle Pond.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int r;
+    float pi=3.14159265358979323846264338327950288419716939937510,b;
+    cin>>r;
+    b=r;
+    printf("%.20f",2*pi*b);
+    return 0;
+}
