7063cdb9cb9c87933233658bdc4dfdea0277c48c
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2020-05-09 15:15:34 +0600

    implementation

---
+++ A - Sheep and Wolves.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a>b)
+        cout<<"safe"<<endl;
+    else
+        cout<<"unsafe"<<endl;
+    return 0;
+}
