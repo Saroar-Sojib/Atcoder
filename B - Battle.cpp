7a404a7a32174d8ea636cb1747a6fe76d35e7055
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2020-05-09 15:19:40 +0600

    greedy

---
+++ B - Battle.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+	while(1) {
+		c = c - b;
+		if (c<=0) {
+			cout << "Yes" << endl;
+			return 0;
+		}
+		a = a - d;
+		if (a<=0) {
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+    return 0;
+}
